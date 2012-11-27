6db5e08fab2f58f41b73d975c9ee4d98cc898621
Kenneth Powers <mail@kenpowers.net>
2012-11-26 23:38:17 -0500

    Implemented tab moving functionality.

--- popup.js
+++ popup.js
@@ -1,16 +1,38 @@
-// Get all tabs in current window
-chrome.tabs.getAllInWindow(null, function (tabs) {
-  // Create and append header
-  var h1 = document.createElement('h1');
-  h1.innerHTML = 'Open Tabs:';
-  document.body.appendChild(h1);
-  // Create list
-  var ul = document.createElement('ul'), li = null;
-  for (var i = 0; i < tabs.length; i++) {
-    li = document.createElement('li');
-    li.innerHTML = tabs[i].title;
-    ul.appendChild(li);
-  }
-  // Append list
-  document.body.appendChild(ul);
+// Input element
+var queryBox = document.getElementById('query');
+
+// Listen for clicks on the new window button
+document.getElementById('new-window').addEventListener('click', function (e) {
+  e.preventDefault();
+  getMatchingTabs(queryBox.value, function (tabs) {
+    chrome.windows.create(function (window) {
+      chrome.tabs.move(tabs, {index: 0, windowId: window.id});
+    });
+  });
 });
+// Listen for clicks on the move to beginning button
+document.getElementById('move-beginning').addEventListener('click', function (e) {
+  e.preventDefault();
+  getMatchingTabs(queryBox.value, function (tabs) {
+    chrome.tabs.move(tabs, {index: 0});
+  });
+});
+
+// Get all tabs which match given query
+function getMatchingTabs(query, callback) {
+  var re;
+  if (/^\/.+\/$/.test(query)) {
+    re = new RegExp(query.substring(1, query.length - 1));
+  } else {
+    re = new RegExp(query.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&'));
+  }
+  var matches = [];
+  chrome.tabs.getAllInWindow(null, function (tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      if (re.test(tabs[i].url)) {
+        matches.push(tabs[i].id);
+      }
+    }
+    callback(matches);
+  });
+}
